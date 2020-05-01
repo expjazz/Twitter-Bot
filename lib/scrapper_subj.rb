69a60b1d4cd72d5e9dac36a207a0fc677935194b
Expedito Andrade <expeditojazz@gmail.com>
2020-05-01 12:35:15 -0300

    fixed main web-scrapper

--- lib/scrapper_subj.rb
+++ lib/scrapper_subj.rb
@@ -3,30 +3,27 @@
 require 'nokogiri'
 require 'httparty'
 require 'pry'
-
+require 'selenium-webdriver'
 class Scrapper
   attr_reader :subject, :unparsed_page, :parsed_page
 
   def initialize(subject)
-    temp = subject.split('')
-    @subject = temp.join('+')
-    @unparsed_page = HTTParty.get("https://www.google.com/search?q=#{@subject}")
-    @doc = Nokogiri::HTML(@unparsed_page)
-
-    @parsed_page = Nokogiri::HTML(unparsed_page)
+    @subject = subject
+    @driver = Selenium::WebDriver.for :firefox
+    @driver.get "https://www.google.com/search?q=#{@subject}"
+    @doc = Nokogiri::HTML @driver.page_source
   end
 
-  def get_title
-    title = @doc.css('div.BNeawe')[0].text
-    title
+  def get_text
+    @doc.css('h3.LC20lb')[0].text
   end
 
   def get_link
-    link = @doc.css('div.kCrYT')[0].child.attributes['href'].value
-    link[7, link.size]
+    @doc.css('div.r').children[0].attributes['href'].value
   end
 end
 
-test = Scrapper.new('Best books to read')
-p test.get_title
-p test.get_link
+# test = Scrapper.new('Best clothes for the whole summer')
+
+# p test.get_text
+# p test.get_link
--- lib/test.rb
+++ lib/test.rb
@@ -2,10 +2,19 @@
 
 require_relative '../keys.rb'
 require 'twitter'
+require_relative 'scrapper_subj.rb'
 
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = Consumer_key
   config.consumer_secret     = Consumer_secret
   config.access_token        = Access_token
   config.access_token_secret = Access_token_secret
+end
+scr = Scrapper.new('Clothes for winter')
+
+posts = [scr.get_title, scr.get_link]
+
+posts.each do |post|
+  p client.update(post)
+  sleep(60)
 end
