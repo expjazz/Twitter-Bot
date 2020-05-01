c0a9e4040e08d771dbe20831d294d13f2232b0a9
Expedito Andrade <expeditojazz@gmail.com>
2020-05-01 09:23:59 -0300

    added weather getter

---
+++ lib/weather.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative '../keys.rb'
+require 'httparty'
+require 'openssl'
+require 'json'
+
+class Weather
+  attr_reader :temperature, :main_description, :details_description, :weather_id
+  def initialize
+    request = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?id=2270968&units=metric&APPID=#{Weather_key}")
+    response = JSON.parse(request.body)
+    @temperature = response['list'][0]['main']['temp']
+    @main_description = response['list'][0]['weather'][0]['main']
+    @details_description = response['list'][0]['weather'][0]['description']
+    @weather_id = response['list'][0]['weather'][0]['id']
+  end
+end
+
+weather = Weather.new
